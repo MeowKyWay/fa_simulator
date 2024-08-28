b1c6db469373ec42533b452ff72773b1bc5f11a9
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-08-28 10:29:31 +0700

    Remove unuse class and add new todo class action_history

---
+++ lib/action_history.dart
@@ -0,0 +1,6 @@
+// Todo implement a class that store all action e.g. create a state delete a state
+// Use the class to undo and redo actions
+// Maybe use interface to implement the actions
+// All class need an undo redo and do function
+// do is the function that is called when the action is done
+// Change all the functions to use the class do function
