4623342fa4b0e74c4be26fbc98b738d51374a708
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-08-30 16:14:06 +0700

    Fixed vocab error on some comment

--- lib/action/focus/add_focus_action.dart
+++ lib/action/focus/add_focus_action.dart
@@ -1,3 +1,3 @@
 //TODO implement add focus action
 //Use to add focus on a state or multiple states
-//unredoable
+//undoable
--- lib/action/focus/focus_action.dart
+++ lib/action/focus/focus_action.dart
@@ -1,3 +1,3 @@
 //TODO implement focus states action
 //Use to request focus on a state or multiple states
-//unredoable
+//undoable
