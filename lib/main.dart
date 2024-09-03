5ac760b24e718135b711f7d9bd17991007049c1f
meow <69355934+MeowKyWay@users.noreply.github.com>
2024-09-03 13:23:55 +0700

    Remove unuse import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:fa_simulator/config/config.dart';
 import 'package:fa_simulator/config/theme.dart';
 import 'package:fa_simulator/widget/body/body_singleton.dart';
 import 'package:fa_simulator/widget/diagram/draggable/feedback_position_provider.dart';
--- lib/widget/component/text.dart
+++ lib/widget/component/text.dart
@@ -1,4 +1,3 @@
-import 'package:fa_simulator/config/config.dart';
 import 'package:fa_simulator/config/theme.dart';
 import 'package:flutter/material.dart';
 
