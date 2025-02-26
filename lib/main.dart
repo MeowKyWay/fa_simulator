03d63ed3ab427567c05018ac8a03ff2f03da52f4
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-02-26 21:31:16 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,12 @@ import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  // runZonedGuarded(() {
-  //   Get.put(SnackbarProvider());
-  //   runApp(Main());
-  // }, (error, stackTrace) {
-  //   Get.find<SnackbarProvider>().showError(error.toString());
-  // });
-  Get.put(SnackbarProvider());
-  runApp(Main());
+  runZonedGuarded(() {
+    Get.put(SnackbarProvider());
+    runApp(Main());
+  }, (error, stackTrace) {
+    Get.find<SnackbarProvider>().showError(error.toString());
+  });
 }
 
 class Main extends StatefulWidget {
